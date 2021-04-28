8f32a5737659bb21f75675d5b745d09c7866deba
HenryQuillin <henryquillin@gmail.com>
2021-04-28 18:33:19 -0500

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class fizzBuzz
+    {
+        public static string[] FizzBuzz()
+        {
+            string[] result = new string[100]; 
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result[i - 1] = "FizzBuzz";  
+                }
+                else if (i % 3 == 0)
+                {
+                    result[i - 1] = "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    result[i - 1] = "Buzz";
+                }
+                else
+                {
+                    result[i - 1] = Convert.ToString(i);
+                }
+
+            }
+            return result;
+        }
+    }
+}
---
+++ FizzBuzzTerminal.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+	class FizzBuzzTerminal
+	{
+        public static void Main(string[] args) {
+            fizzBuzz newFizzBuzz = new fizzBuzz();
+            string[] array = fizzBuzz.FizzBuzz();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadLine(); 
+
+        }
+
+    }
+
+}
